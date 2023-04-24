2726eff706d8c85fd9b6832e9c43eb0549993e3d
Zachary Blackwood <zachary@streamlit.io>
2023-04-24 15:11:07 -0400

    Add tooltips

---
+++ pages/tooltips.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Hello, world!", help="This is a help message")
+
+st.header("This is a header", help="This is a help message")
+
+st.text(
+    """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua.
+    """,
+    help="This is a help message",
+)
