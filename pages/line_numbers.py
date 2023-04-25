a4bf48cdd2fa175c2cf129761c931d95f46574e4
Zachary Blackwood <zachary@streamlit.io>
2023-04-25 16:25:40 -0400

    Allow changing width

--- pages/line_numbers.py
+++ pages/line_numbers.py
@@ -2,12 +2,21 @@ import streamlit as st
 
 line_numbers = st.checkbox("Show line numbers", value=True)
 
-st.code(
-    """
+width = st.slider("Code width", min_value=1, max_value=20, value=10)
+
+_, col, _ = st.columns([1, width, 1])
+
+col.code(
+    f"""
 import streamlit as st
 
-st.code("Insert your python code here!", language="python", line_number = True)
+st.write("Hello, world!")
 
+st.code(
+    "Insert your python code here!",
+    language="python",
+    line_numbers={line_numbers},
+)
 """,
     language="python",
     line_numbers=line_numbers,
